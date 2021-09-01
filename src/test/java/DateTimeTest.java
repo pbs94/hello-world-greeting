39d9f93fda7bbf6d3032d3cdbeb6efbb37c79c27
박병설 <scipio.airl@navercorp.com>
2021-09-01 16:21:09 +0900

    GitHub 연동 테스트

--- src/test/java/DateTimeTest.java
+++ src/test/java/DateTimeTest.java
@@ -15,7 +15,5 @@ public class DateTimeTest {
       GregorianCalendar time = new GregorianCalendar();
       int hour = time.get(Calendar.HOUR_OF_DAY);
       assertEquals(hour, var.dateTime());
-
-      
    }
 }
